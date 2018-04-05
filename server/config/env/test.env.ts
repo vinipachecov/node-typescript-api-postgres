7363843a1efc02c5c1e6019784dbd8753fd81ff4
vinicius p vieira <vinipachecov@gmail.com>
2018-04-05 08:42:01 -0300

    configs

---
+++ server/config/env/test.env.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  env: 'test',
+  db: 'ts-api-test',
+  dialect: 'postgres',
+  username: 'postgres',
+  password: '123456',
+  host: 'localhost',
+  serverPort: 3000,
+  pgPort: 5432,
+  dbURL: 'postgres://postgres:123456@localhost:5432/ts_api_test',
+  secret: 'Secret'
+}
